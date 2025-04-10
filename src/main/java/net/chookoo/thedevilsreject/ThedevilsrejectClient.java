9a3e79994e6e9686707ee453764ebfe65cee537a
Chookoo <Chookoo1112@gmail.com>
2025-04-10 15:17:46 +1000

    Initial commit

---
+++ src/main/java/net/chookoo/thedevilsreject/ThedevilsrejectClient.java
@@ -0,0 +1,10 @@
+package net.chookoo.thedevilsreject;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThedevilsrejectClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
